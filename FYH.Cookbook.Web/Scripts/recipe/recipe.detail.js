0278d123f72cb4b93c56d0953e4f694ef3a029a9
fanyinghao <i20132013>
2016-02-23 12:43:07 +0800

    add print and pdf function

--- FYH.Cookbook.Web/Scripts/recipe/recipe.detail.js
+++ FYH.Cookbook.Web/Scripts/recipe/recipe.detail.js
@@ -1,12 +1,39 @@
-﻿$(function() {
+﻿var downloadPdf = function () {
+    var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    doc.fromHTML($(".div-to-preview").html() + $("footer").html(), 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+    });
+
+    doc.save($("#Name").val() + ".pdf");
+}
+
+$(function () {
     $("#btnDel").on({
         click: function () {
-            confirm("Are you sure to delete this recipe?", function(e) {
+            confirm("Are you sure to delete this recipe?", function (e) {
                 if (e) {
                     window.location = $("#btnDel").attr('href');
                 }
             });
             return false;
+        }
+    });
+
+    $("#btnPreview").on({
+        click: function () {
+            var btns = '<div class="row">' +
+                '<div class="col-lg-2"><button class="btn btn-lg btn-danger" id="btnPrint" onclick="window.print();">Print</button></div>' +
+                '<div class="col-lg-2"><button class="btn btn-lg btn-danger" id="btnDownload" onclick="downloadPdf();">Download</button></div>' +
+                '<div class="col-lg-2"><button class="btn btn-lg btn-default" data-dismiss="modal">Cancel</button></div>' +
+                '</div></div>';
+            showModal("Print Preview", "<div class='print-hide'>" + $(".div-to-preview").html().replace(/col-lg-8/g, "col-lg-12") + btns + "</div>");
         }
     });
 });
